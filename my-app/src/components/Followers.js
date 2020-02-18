9a3d6711f77f4c5c6fbf77d88b60b64a4ac1a823
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-02-18 09:37:59 -0800

    mvp

--- my-app/src/components/Followers.js
+++ my-app/src/components/Followers.js
@@ -1,11 +1,34 @@
 import React from "react";
+import axios from "axios";
 
 class Followers extends React.Component {
+
+    state = {
+        followers: []
+    }
+
+    componentDidMount() {
+        axios
+            .get("https://api.github.com/users/nasraaden/followers")
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    followers: res.data
+                })
+            })
+            .catch(err => console.log(err))
+    }
 
     render(){
         return(
             <div>
-                My Followers Github Cards
+                <h2>My Followers Github Cards</h2>
+                {this.state.followers.map(follower => (
+                    <div key={follower.id}>
+                        <img src={follower.avatar_url}/>
+                        <h4>{follower.login}</h4>
+                    </div>
+                ))}
             </div>
         )
     }
--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -1,11 +1,31 @@
 import React from "react";
+import axios from "axios"
 
 class User extends React.Component {
-
+    state = {}
+    
+      componentDidMount() {
+        axios
+          .get("https://api.github.com/users/nasraaden")
+          .then(res => {
+              console.log(res)
+            this.setState({
+                img: res.data.avatar_url,
+                name: res.data.name,
+                bio: res.data.bio,
+                location: res.data.location
+            })
+          })
+          .catch(err => console.log(err))
+      }
     render(){
         return(
             <div>
-                My Github Card
+                <h2>My Github User Card</h2>
+                <img src={this.state.img} />
+                <h3>{this.state.name}</h3>
+                <p>{this.state.bio}</p>
+                <p>{this.state.location}</p>
             </div>
         )
     }
